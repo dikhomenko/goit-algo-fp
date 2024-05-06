01fd488f4a060f0717607fd5ae7c12037e756864
dikhomenko <dinamastercs@gmail.com>
2024-05-06 16:14:26 +0200

    Dijkstra's algorithm for finding the shortest paths in a weighted graph added

---
+++ Task3.py
@@ -0,0 +1,55 @@
+import heapq
+import sys
+
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.graph = {i: [] for i in range(vertices)}
+
+    def add_edge(self, u, v, weight):
+        self.graph[u].append((v, weight))
+
+
+def dijkstra(graph, start_vertex):
+    distances = {vertex: float('infinity') for vertex in range(graph.vertices)}
+    distances[start_vertex] = 0
+    priority_queue = [(0, start_vertex)]
+
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+
+        # Nodes can get added to the priority queue multiple times. We only
+        # process a vertex the first time we remove it from the priority queue.
+        if current_distance > distances[current_vertex]:
+            continue
+
+        for neighbor, weight in graph.graph[current_vertex]:
+            distance = current_distance + weight
+
+            # Only consider this new path if it's better
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    
+    return distances
+
+
+g = Graph(9)
+g.add_edge(0, 1, 4)
+g.add_edge(0, 7, 8)
+g.add_edge(1, 2, 8)
+g.add_edge(1, 7, 11)
+g.add_edge(2, 3, 7)
+g.add_edge(2, 8, 2)
+g.add_edge(2, 5, 4)
+g.add_edge(3, 4, 9)
+g.add_edge(3, 5, 14)
+g.add_edge(4, 5, 10)
+g.add_edge(5, 6, 2)
+g.add_edge(6, 7, 1)
+g.add_edge(6, 8, 6)
+g.add_edge(7, 8, 7)
+
+distances = dijkstra(g, 0)
+for vertex in range(len(distances)):
+    print(f"Distance from vertex 0 to vertex {vertex} is {distances[vertex]}")
